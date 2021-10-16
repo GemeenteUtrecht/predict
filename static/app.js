ed5d9c949a0da84a5220374900a6bd56635ecd5b
Olling <48805670+Ollie-nl@users.noreply.github.com>
2021-10-16 18:05:24 +0200

    Update background color switcher

--- static/app.js
+++ static/app.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const textInput = document.getElementById('textInput')
     const predictionResult = document.getElementById('predictionResult')
+    const setBodyResult = document.body;
+    var MyClass; 
 
     textInput.addEventListener('input', async (e) => {
         const response = await fetch('/api/predict', {
@@ -10,5 +12,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const data = await response.json()
             predictionResult.innerHTML = `<em class="font${Math.round(data.probability*10)}">${Math.round(data.probability*10000)/100}%</em> kans dat de bovenstaande tekst een <em class="${data.label}">${data.label}</em> is`
+            
+
+            MyClass = `font${Math.round(data.probability*10)}`
+           //alert(MyClass);
+            //setBodyResult.classList.remove("foo");
+            setBodyResult.className="";
+            setBodyResult.classList.add(MyClass);
     })
 })
